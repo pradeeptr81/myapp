ec29c5b2bca52f0ac13764cee66058ef5ca6d38d
root <root@HD-TEJ-52.localdomain>
2025-10-06 15:42:50 +0400

    Fix package.json syntax

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+// app.js
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello CI/CD!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app; // export app for testing
 
